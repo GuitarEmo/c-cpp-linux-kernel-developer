ebaefccb700b05c063d91bb5d948aa41ecd5e2bd
Nikita <p.nikita@wanted.ooo>
2025-08-29 13:41:12 +0300

    Update driver.c

--- driver.c
+++ driver.c
@@ -1,7 +1,13 @@
-// driver.c - Example C file for Linux driver vacancy
 #include <stdio.h>
+
+void callToAction() {
+    printf("Откликайся скорее!\n");
+}
 
 int main() {
     printf("This is a placeholder for Linux driver code.\n");
+    callToAction();
+    printf("Не пропусти возможность!\n");
+    callToAction();
     return 0;
 }
