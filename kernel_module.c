4b0d1e08c0b8539afee44712a26c025df393fb0e
Nikita <p.nikita@wanted.ooo>
2025-08-29 13:41:52 +0300

    Update kernel_module.c

--- kernel_module.c
+++ kernel_module.c
@@ -1,15 +1,20 @@
-// kernel_module.c - Example Linux Kernel Module
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+static void callToAction(void) {
+    printk(KERN_INFO "Откликайся скорее, Linux разработчик!\n");
+}
+
 static int __init hello_init(void) {
     printk(KERN_INFO "Hello, Linux kernel module loaded!\n");
+    callToAction();
     return 0;
 }
 
 static void __exit hello_exit(void) {
     printk(KERN_INFO "Goodbye, Linux kernel module unloaded!\n");
+    callToAction();
 }
 
 module_init(hello_init);
